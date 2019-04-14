27a0cc3731938591a713c3015b10b735f44bd120
Patriot1994 <nowikpawel1994@gmail.com>
2019-04-14 17:24:19 +0200

    first commit

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var http = require('http');
 var fs = require('fs');
-var index = fs.readFileSync('index.html');
+
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
+  res.end("listen on 8080");
 }).listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
