5cf33cd332b3fea164c4281be70e1a7eacdbcbad
Patriot1994 <nowikpawel1994@gmail.com>
2019-04-14 17:11:22 +0200

    first commit

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-// content of index.js
-const http = require('http')
-const port = 3000
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
